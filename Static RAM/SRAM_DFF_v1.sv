6383c1e2995d674ecdb88a9d6723310d3935636a
omkarpradhan <omkarpradhan@users.noreply.github.com>
2017-02-19 12:16:48 -0700

    Update and rename SRAM_DFF.sv to SRAM_DFF_v1.sv

--- Static RAM/SRAM_DFF_v1.sv
+++ Static RAM/SRAM_DFF_v1.sv
@@ -1,8 +1,17 @@
 // Author		: Omkar Pradhan
 // Date  		: 2/172017
-// Description	: A D-FF based 8 x 32 bit memory (SRAM)
+// Description	: A D-FF based 8 x 32 bit memory (SRAM).
+//		  Decoding, memory logic and multiplexing are all included in this single module
+/*
+I/O: 
+	we -  Write Enable, asserting this low disables access to the registers
+		  and previous memory is stored. Pass to the memory module
+	add - ADDress, address bus to select memory location. Pass to decoder module.
+	wd -  Write Data, 32 bit data bus for writing to SRAM. Pass to memory module.
+	rd -  Read Data, 32 bit data bus for reading from SRAM.Read from mux module.
+*/
 
-module SRAM_DFF(
+module SRAM_DFF_v1(
 input logic[2:0] add,//address
 input logic		 we, //write enable 
 input logic		 clk,
@@ -21,8 +30,11 @@ logic wen0,wen1,wen2,wen3,wen4,wen5,wen6,wen7;// word enables to select between 
 logic[31:0] reg0,reg1,reg2,reg3,reg4,reg5,reg6,reg7;//32 bit outputputs
 logic[31:0] reg0_next,reg1_next,reg2_next,reg3_next,reg4_next,reg5_next,reg6_next,reg7_next;//32 bit inputs.
 
-//Memory logic - each of the 8 memory registers are made up of 32 D-FF
-//hw - D-FFs
+//------------------------Memory logic ------------------------------
+//1. Synchronous data latching
+//Data is clocked in at rising edge of each clock pulse only
+// each of the 8 memory registers are made up of 32 bit registers
+//hw - D-FFs make up each memory cell
 always_ff @(posedge clk or negedge resetn)//create the memory flip flips. Each regx is made up of 32 D-ff
 	if(resetn==1'b0) begin//if reset is asserted low then store 0
 		reg0 <= #1 32'b0;
@@ -43,8 +55,10 @@ always_ff @(posedge clk or negedge resetn)//create the memory flip flips. Each r
 		reg6 <= #1 reg6_next;
 		reg7 <= #1 reg7_next;
 	end
-//----------------------Data storage loop-------------------------
-// This logic uses the set/reset wenX to update or keep memory bit.	
+//2. Data storage loop
+// This logic uses the wen[X] to update or keep memory bit.	
+// The previous data bits are looped in register X when wen[X] is 0.
+// New data bits on wd are stored in register X when wen[X] is 1.
 //hw - 2x1 mux for each memory cell
 always_comb begin//bit is stored in the loop bet. Q and D when we is asserted low
 //use of conditional operator (condition)? value1:value2
@@ -72,7 +86,7 @@ always_comb begin
 	wen7 = add[2] & add[1] & add[0] & we;
 end//always_comb
 
-//---------Read logic address delayed address latch-------------------------
+//---------Read logic delayed address latch-------------------------
 //this FF will delay the address change on address bus before being passed onto the read mux.
 //HW - 3 x D-FF
 always_ff @(posedge clk or negedge resetn)
@@ -91,16 +105,14 @@ always_comb begin
 	//The only condition for read is that, once we is asserted low then the read address should be latched at the next clk posedge. 
 	//if the read cycle is to be dissabled during the write cycle then put the following statements inside an if-else loop which will 
 	//result in a enable/disable buffer.
-		rd = (add_rd == 3'b000)? reg0:
-			 ((add_rd == 3'b001)? reg1:
-			 ((add_rd == 3'b010)? reg2:
-			 ((add_rd == 3'b011)? reg3:
-			 ((add_rd == 3'b100)? reg4:
-			 ((add_rd == 3'b101)? reg5:
-			 ((add_rd == 3'b110)? reg6:
+	rd = (add_rd == 3'b000)? reg0:
+		 ((add_rd == 3'b001)? reg1:
+		 ((add_rd == 3'b010)? reg2:
+		 ((add_rd == 3'b011)? reg3:
+		 ((add_rd == 3'b100)? reg4:
+		 ((add_rd == 3'b101)? reg5:
+		 ((add_rd == 3'b110)? reg6:
 			 ((add_rd == 3'b111)? reg7:32'h0000)))))));
 end//always_comb
-//---------------------------Read logic address decoder ENDS-------------------------
-
 
 endmodule//SRAM_DFF
